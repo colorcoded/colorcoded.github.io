7f629e56d36b674b8a75e5772ab7ddd584ab032d
David Hernandez <dhern167@calpoly.edu>
2024-05-05 01:31:14 -0700

    Build

---
+++ .svelte-kit/output/server/chunks/ProgressBar.svelte_svelte_type_style_lang.js
@@ -0,0 +1,46 @@
+import { w as writable, r as readable } from "./index.js";
+import { l as get_store_value } from "./ssr.js";
+const stores = {};
+function localStorageStore(key, initialValue, options) {
+  options?.serializer ?? JSON;
+  options?.storage ?? "local";
+  if (!stores[key]) {
+    const store = writable(initialValue, (set2) => {
+    });
+    const { subscribe, set } = store;
+    stores[key] = {
+      set(value) {
+        set(value);
+      },
+      update(updater) {
+        const value = updater(get_store_value(store));
+        set(value);
+      },
+      subscribe
+    };
+  }
+  return stores[key];
+}
+localStorageStore("modeOsPrefers", false);
+localStorageStore("modeUserPrefers", void 0);
+const modeCurrent = localStorageStore("modeCurrent", false);
+function setInitialClassState() {
+  const elemHtmlClasses = document.documentElement.classList;
+  const condLocalStorageUserPrefs = localStorage.getItem("modeUserPrefers") === "false";
+  const condLocalStorageUserPrefsExists = !("modeUserPrefers" in localStorage);
+  const condMatchMedia = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  if (condLocalStorageUserPrefs || condLocalStorageUserPrefsExists && condMatchMedia) {
+    elemHtmlClasses.add("dark");
+  } else {
+    elemHtmlClasses.remove("dark");
+  }
+}
+function prefersReducedMotion() {
+  return false;
+}
+readable(prefersReducedMotion(), (set) => {
+});
+export {
+  modeCurrent as m,
+  setInitialClassState as s
+};
---
+++ .svelte-kit/output/server/manifest.js
@@ -0,0 +1,34 @@
+export const manifest = (() => {
+function __memo(fn) {
+	let value;
+	return () => value ??= (value = fn());
+}
+
+return {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set([".DS_Store","favicon.png","officers/.DS_Store","officers/IvanMartinez.jpeg","officers/Samantha_Soto.jpeg","officers/Sarah.jpg","officers/anthonyColin2.png","officers/david.jpeg","officers/foaad.jpg","officers/luke2.png"]),
+	mimeTypes: {".png":"image/png",".jpeg":"image/jpeg",".jpg":"image/jpeg"},
+	_: {
+		client: {"start":"_app/immutable/entry/start.-JazLGaf.js","app":"_app/immutable/entry/app.Q8CBPmrr.js","imports":["_app/immutable/entry/start.-JazLGaf.js","_app/immutable/chunks/scheduler.zQ9XRYPG.js","_app/immutable/chunks/singletons.uBrwG0vV.js","_app/immutable/chunks/index.fA6snhSw.js","_app/immutable/entry/app.Q8CBPmrr.js","_app/immutable/chunks/scheduler.zQ9XRYPG.js","_app/immutable/chunks/index.R2nzb5FK.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		nodes: [
+			__memo(() => import('./nodes/0.js')),
+			__memo(() => import('./nodes/1.js')),
+			__memo(() => import('./nodes/2.js'))
+		],
+		routes: [
+			{
+				id: "/",
+				pattern: /^\/$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 2 },
+				endpoint: null
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+}
+})();
