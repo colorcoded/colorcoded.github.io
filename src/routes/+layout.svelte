48cff7a05fbedc7a99c5fbe2c0b5c9c8bfef2f5b
David Hernandez <dhern167@calpoly.edu>
2024-05-06 08:34:38 -0700

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -29,6 +29,11 @@
 
 </script>
 
+
+<meta http-equiv='cache-control' content='no-cache'> 
+<meta http-equiv='expires' content='0'> 
+<meta http-equiv='pragma' content='no-cache'>
+
 <!-- App Shell -->
 <AppShell>
 	<svelte:fragment slot="header">
