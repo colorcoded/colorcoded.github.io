c1153000e816725f7aca21b53a3056c2e23f67d9
Giovanni De La Luz <delaluz@calpoly.edu>
2025-02-17 18:35:00 -0800

    debug deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  base: 'https://github.com/colorcoded/colorcoded.github.io', 
   build: {
     outDir: 'dist',
   },
